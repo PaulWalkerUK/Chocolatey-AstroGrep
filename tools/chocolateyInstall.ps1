738fcc158e7c9b6bf9f191c3dd5e72c6970eb874
PaulWalkerUK <walker.paul+github@gmail.com>
2017-08-25 01:11:23 +0100

    Updated to AstroGrep 4.4.6

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,3 +1,3 @@
 ﻿$packageName = "AstroGrep"
-$url = 'http://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.5/AstroGrep_Setup_v4.4.5.exe'
+$url = 'https://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.6/AstroGrep_Setup_v4.4.6.exe'
 Install-ChocolateyPackage $packageName 'exe' '/S' $url
