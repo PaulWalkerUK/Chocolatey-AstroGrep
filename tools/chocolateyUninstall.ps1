60ec2a39f41380536e4b3f975dbb8d9f33b96124
PaulWalkerUK <walker.paul+github@gmail.com>
2017-08-27 00:34:45 +0100

    Get the uninstall string from the registry

--- tools/chocolateyUninstall.ps1
+++ tools/chocolateyUninstall.ps1
@@ -1,2 +1,23 @@
 $packageName = "AstroGrep"
-Uninstall-ChocolateyPackage $packageName 'exe' '/S' "C:\Program Files (x86)\AstroGrep\Uninstall.exe"
+$uninstallString = ""
+
+
+#Registry path depends on whether we're asking from a 32 bit or 64 bit process - check both
+$uninstallRegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AstroGrep"
+$uninstallRegPathWOW6432 = "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\AstroGrep"
+
+If (Test-Path $uninstallRegPath) {
+    #Would be cleaner to use `Get-ItemPropertyValue`, but that's not available in PS v2
+    $uninstallString = (Get-ItemProperty -Path $uninstallRegPath -Name UninstallString).UninstallString
+}
+ElseIf (Test-Path $uninstallRegPathWOW6432) {
+    $uninstallString = (Get-ItemProperty -Path $uninstallRegPathWOW6432 -Name UninstallString).UninstallString
+}
+
+
+If ($uninstallString -eq "") {
+    throw "Uninstall details not found in registry"
+}
+
+
+Uninstall-ChocolateyPackage $packageName 'exe' '/S' $uninstallString
