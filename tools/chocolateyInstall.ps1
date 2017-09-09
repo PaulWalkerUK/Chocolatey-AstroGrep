b524fdc5c337cc17efa5003f87a1b4c1e050c47b
PaulWalkerUK <walker.paul+github@gmail.com>
2017-09-09 23:28:28 +0100

    Tidy up capitalisation

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 ﻿$installParams = @{
-    packageName = 'astrogrep'
-    fileType = 'exe'
-    silentArgs = '/S'
-    url = 'https://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.6/AstroGrep_Setup_v4.4.6.exe'
+    PackageName = 'astrogrep'
+    FileType = 'exe'
+    SilentArgs = '/S'
+    Url = 'https://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.6/AstroGrep_Setup_v4.4.6.exe'
     Checksum = 'CD544F508837122389669AE031DD40F71F4147FFC6A4F5948A22DE8BEAAE74C5B3310EBBFCAF4818AF9E279425AEA2D62D0E927933F1B61465D35AB97BCB9DBF'
     ChecksumType = 'sha512'
 }
