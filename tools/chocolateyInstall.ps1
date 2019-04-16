b33e9a6252a6f7a005c12b52fa7d03f0511ae381
PaulWalkerUK <walker.paul+github@gmail.com>
2019-04-16 22:00:54 +0100

    Update to v4.4.7

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
     PackageName = 'astrogrep'
     FileType = 'exe'
     SilentArgs = '/S'
-    Url = 'https://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.6/AstroGrep_Setup_v4.4.6.exe'
-    Checksum = 'CD544F508837122389669AE031DD40F71F4147FFC6A4F5948A22DE8BEAAE74C5B3310EBBFCAF4818AF9E279425AEA2D62D0E927933F1B61465D35AB97BCB9DBF'
+    Url = 'https://downloads.sourceforge.net/project/astrogrep/AstroGrep%20%28Win32%29/AstroGrep%20v4.4.7/AstroGrep_Setup_v4.4.7.exe'
+    Checksum = '8E2FA5F33E16879D8F5ACB4AB783AA4B4B37266CD1346ABEF5D54F2DFEB2177AF872575780E2E7CD02E462349B1C35642C0F7BA3F860034775A064E9A07B08AF'
     ChecksumType = 'sha512'
 }
 
